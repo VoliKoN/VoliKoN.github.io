d3bbabe8d313f5426cc845aeac3d125e620e2137
VoliKoN <volvos.e@gmail.com>
2022-02-12 00:34:18 +0200

    Change to remote theme properly, add comments

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-docker run --rm   --volume="$PWD:/srv/jekyll" -p 4000:4000   -it jekyll/jekyll:latest jekyll server
+docker run --rm --volume="$PWD:/srv/jekyll" -e "JEKYLL_ENV=production" -p 4000:4000 -it jekyll/jekyll:latest jekyll serve --verbose --trace
 
+# docker run --rm --volume="$PWD:/srv/jekyll" -e "JEKYLL_ENV=production" -p 4000:4000 -it jekyll/jekyll:latest bash
